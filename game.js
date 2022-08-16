bcd5633b18dc19c417b02a6f4739561281c3439e
Ghufran <ghufranshahid101@gmail.com>
2022-08-16 01:15:17 -0400

    Added if statements to determine which symbol the computer would play

--- game.js
+++ game.js
@@ -1,6 +1,20 @@
 function getComputerChoice (){
 
     let choice = Math.floor(Math.random() * 3) + 1;
-    return choice;
+    let result;
+
+    if (choice == 1){
+        result = 'rock';
+    }
+    else if(choice == 2){
+        result = 'paper';
+    }
+    else{
+        result = 'scissors'
+    }
 }
 console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection){
+
+}
